e2df68e67e6e7f947607218b0517128f70a485d2
Unknown <squidey57@gmail.com>
2018-03-26 16:24:36 +0100

    One of the ghosts move, in a simple rectangle but it's a start

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMove : MonoBehaviour
+{
+    public Transform[] _waypoints;
+    int _cur = 0;
+
+    public float _speed = 0.3f;
+
+    void FixedUpdate()
+    {
+        // Waypoint not reached yet? then move closer
+        if (transform.position != _waypoints[_cur].position)
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position,
+                _waypoints[_cur].position,
+                _speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        // Waypoint reached, select next one
+        else _cur = (_cur + 1) % _waypoints.Length;
+    }
+}
